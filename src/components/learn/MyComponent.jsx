24a026d0f5bd82351bc71f1f2f9382f37fc41321
DucHuy1411 <huu13bn@gmail.com>
2025-10-30 23:45:59 +0700

    29

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,11 @@
+const FakeComponent = () => {
+  return(
+    <div> FakeComponent </div>
+  );
+}
+const MyComponent = () => {
+  return(
+    <div> DucHuy && EPU </div>
+  );
+}
+export default MyComponent;
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,15 @@
+const ThirdComponent = () => {
+return(
+    <div>
+        ThirdComponent
+    </div>
+)
+}
+const SecondComponent = () => {
+return(
+    <div>
+        SecondComponent
+    </div>
+)
+}
+export {SecondComponent, ThirdComponent};
